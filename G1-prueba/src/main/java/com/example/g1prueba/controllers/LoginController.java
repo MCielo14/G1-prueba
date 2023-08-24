256105a048aed6c6d5042860670f989cabeed630
= <=>
2023-08-23 22:54:35 -0500

    a

---
+++ G1-prueba/src/main/java/com/example/g1prueba/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.g1prueba.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String empleado(){
+        return "login";
+    }
+}
