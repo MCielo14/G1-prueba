364d9580e1e0e733af82a757ae43314743209307
= <=>
2023-08-23 21:43:15 -0500

    se creo el proyecto

---
+++ G1-prueba/src/main/java/com/example/g1prueba/G1PruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.g1prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1PruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G1PruebaApplication.class, args);
+    }
+
+}
---
+++ G1-prueba/src/test/java/com/example/g1prueba/G1PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g1prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1PruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
